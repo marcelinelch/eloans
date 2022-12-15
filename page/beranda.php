fab6bf9995c36625c3d4673ecb6df1210ee8c56e
marcelinelch <marcelinelch@gmail.com>
2022-12-15 12:45:39 +0800

    first commit

---
+++ page/beranda.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <!-- Page title -->
+    <div class="page-header d-print-none">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <!-- Page pre-title -->
+                <div class="page-pretitle">
+                    Overview
+                </div>
+                <h2 class="page-title">
+                    Dashboard
+                </h2>
+            </div>
+            <!-- Page title actions -->
+
+        </div>
+    </div>
+</div>
+
+
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-10">
+                                <h3 class="h1">Halo <?= $_SESSION['level'] ?>, Selamat Datang Di E-Loans Aplikasi Sistem Pendukung Keputusan Kelayakan Karyawan Mendapat Pinjaman</h3>
+                                <div class="markdown text-muted">
+                                    Aplikasi E-Loans ini merupakan aplikasi khusus untuk menentukan kelayakan karyawan dalam mendapatkan pinjaman . Penentuan karyawan yang mendapatkan pinjaman ini menggunakan metode Sistem pendukung keputusan (SPK) Simple Additive Weighting (SAW). Dengan adanya aplikasi ini dapat mempermudah perusahaan dalam menentukan karyawan mendapat pinjaman secara otomatis tanpa dihitung manual oleh HRD.</div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
